5dc633b6b675dfa41d1f01193bc96314a3a2c196
kpomier777 <kevinpomier.kp@gmail.com>
2020-08-10 23:19:27 -0400

    lifecycle

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,29 +1,49 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, DoCheck, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-test1',
   templateUrl: './test1.component.html',
   styleUrls: ['./test1.component.scss']
 })
-export class Test1Component implements OnInit {
+export class Test1Component implements OnInit, DoCheck, OnChanges, AfterContentInit,AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
 
-  @Input() name:string ;
-  @Input() age:string ;
-  @Input() description:string ;
+  @Input() name: string = "kpomier";
 
-
-    @Output() clickSave = new  EventEmitter();
-
+  
+  
+    
   constructor() { }
 
   ngOnInit() {
+    console.log('On Init')
   }
-  onClickSave(){
-    this.clickSave.emit({
-      name: this.name,
-      age: this.age,
-      description: this.description
-    });
+
+  ngOnChanges(){
+    console.log('On change')
+  }
+  ngDoCheck(){
+    console.log('On check')
+  }
+
+  ngAfterContentInit(){
+    console.log("AfterContentInit")
+  }
+  ngAfterContentChecked(){
+    console.log("AfterContentChecked")
+  }
+  ngAfterViewInit(){
+    console.log("AfterViewInit")
+  }
+  ngAfterViewChecked(){
+    console.log("AfterViewChecked")
+  }
+  ngOnDestroy(){
+    console.log("Destroy")
+  }
+
+
+
+    
   }  
-}
+
