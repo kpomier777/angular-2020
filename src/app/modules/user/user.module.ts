970dcd3211a870b30593ec898f6ee6771ba737a2
kpomier777 <kevinpomier.kp@gmail.com>
2020-08-24 21:22:04 -0400

    childModuleRuting

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutes } from './user.routing';
 import {UsercomComponent} from './usercom/usercom.component'
+import {User1Component} from './user1/user1.component'
+import {User2Component} from './user2/user2.component'
 
 @NgModule({
   imports: [
     CommonModule,
     UserRoutes
   ],
-  declarations: [UsercomComponent]
+  declarations: [UsercomComponent,User1Component,
+    User2Component]
 })
 export class UserModule { }
