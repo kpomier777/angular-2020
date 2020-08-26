5db8a56a5a821277a37a27e7aed650d25ed8ac36
kpomier777 <kevinpomier.kp@gmail.com>
2020-08-26 17:35:00 -0400

    materializeConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatSliderModule } from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import {HomeComponent} from './components/home/home.component'
 import {AdminComponent} from './components/admin/admin.component'
 import {AboutComponent} from './components/about/about.component'
 import {Home1Component} from './components/home1/home1.component'
 import {Home2Component} from './components/home2/home2.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +27,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
