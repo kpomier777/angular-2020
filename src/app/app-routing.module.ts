6992cff2b476acafed6cb88ee562fb08f54b44f5
kpomier777 <kevinpomier.kp@gmail.com>
2020-08-26 18:45:19 -0400

    PedidosYaCards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full'},
   { path: 'home',loadChildren: ()=> import('./pages/home/home.module').then(n => n.HomeModule) },
   { path: 'admin',loadChildren: ()=> import('./pages/admin/admin.module').then(n => n.AdminModule) },
   { path: 'list',loadChildren: ()=> import('./pages/list/list.module').then(n => n.ListModule) }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomeComponent} from './components/home/home.component'
 import { HomeRoutingModule } from './home-routing.module';
-
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class HomeModule { }
