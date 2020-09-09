0b783af087a825fccca1ff4f4c464a6b5e72fd49
kpomier777 <kevinpomier.kp@gmail.com>
2020-09-09 14:58:39 -0400

    leaf and local storage

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 
@@ -13,7 +14,7 @@ constructor(private http: HttpClient) { }
 
 public login(body: any): Observable<any> {
   return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.key}`, body).pipe(
-    map((res: any) =>{ 
+    map( (res: any) =>{ 
       this.authSucces(res.idToken);
       return res
     })
